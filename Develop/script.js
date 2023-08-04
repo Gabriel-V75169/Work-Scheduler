89dbb36c0fd49f1b5d56d2feb42cb461de4d6b32
Gabriel Valencia <jrjr75169@gmail.com>
2023-08-03 18:31:21 -0700

    jst added some new stuff still doesnt work

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,13 @@
 // in the html.
 // var saveTask = document.getElementById('#saveBtn');
 
+var array = [];
+$('div').each(function(){
+    var id = $(this).attr('id');
+    array.push(id);
+});
+ 
+
 $(document).ready(function(){
 
   // TODO: Add a listener for click events on the save button. This code should
@@ -13,38 +20,35 @@ $(document).ready(function(){
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   $(".saveBtn").click(function() {
-    const theTime = $(this).parent('.time-block').attr("id");
+    var theTime = $(this).parent('.time-block').attr("id");
     
     var textPlace = document.querySelector('.description').value;
     localStorage.setItem(theTime, JSON.stringify(textPlace));
+    
+    window.onload = function () {
+    for (let i = 0; i < id.length; i++)
+    if (id[i] == localStorage.key){
+      document.id[i].children(".description").innerHTML = localStorage.value
+    }
+  };
   });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  function timeManagment() {
-    const arr = ['#hour-1, #hour-2, #hour-3, #hour-4, #hour-5, #hour-6, #hour-7, #hour-8, #hour-9 #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17, #hour-18, #hour-19, #hour-20, #hour-21, #hour-22, #hour-23, #hour-24'];
-    
-    if (time > arr[x]) {
-      .classList.add("past");
-    } else if (time == arr[x]) {
-      .classList.add("present");
-    } else {
-      .classList.add("future");
-    };
-  };
-
+  
+   
   
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  window.onload = function() {
-    
-  }
+
+  
+
   // TODO: Add code to display the current date in the header of the page.
 function updatingTime() {
-  const currentTime = dayjs().format('MMM D HH:mm:ss');
+  const currentTime = dayjs().format('H');
   document.getElementById('currentDay').innerHTML = currentTime;
 };
 setInterval(updatingTime, 1000);
@@ -55,3 +59,5 @@ setInterval(updatingTime, 1000);
 
 
 
+// function timeManagment() {
+//   const arr = ['#hour-1', '#hour-2', '#hour-3', '#hour-4', '#hour-5', '#hour-6', '#hour-7', '#hour-8', '#hour-9', '#hour-10', '#hour-11', '#hour-12', '#hour-13', '#hour-14', '#hour-15', '#hour-16', '#hour-17', '#hour-18', '#hour-19', '#hour-20', '#hour-21', '#hour-22', '#hour-23', '#hour-24'];
